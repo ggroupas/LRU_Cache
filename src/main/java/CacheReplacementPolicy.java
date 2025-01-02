b3bf17e23739f305772e664471f8c1b3035407ab
Giorgos Roupas <groupas.dev@gmail.com>
2025-01-02 05:42:07 +0200

    Create CacheReplacementPolicy.java

---
+++ src/main/java/CacheReplacementPolicy.java
@@ -0,0 +1,19 @@
+public enum CacheReplacementPolicy {
+    LRU("Least Recently Used"),
+    MRU("Most Recently Used"),
+    LFU("Least Frequently Used");
+    private final String description;
+
+    CacheReplacementPolicy(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
