c6784dcb48b193a63ebea9ccb50fced2cfaff413
Giorgos Roupas <groupas.dev@gmail.com>
2025-01-02 07:29:17 +0200

    Update CacheReplacementPolicy.java

--- src/main/java/CacheReplacementPolicy.java
+++ src/main/java/CacheReplacementPolicy.java
@@ -1,15 +1,15 @@
+import lombok.Getter;
+
 public enum CacheReplacementPolicy {
     LRU("Least Recently Used"),
     MRU("Most Recently Used"),
     LFU("Least Frequently Used");
+
+    @Getter
     private final String description;
 
     CacheReplacementPolicy(String description) {
         this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     @Override
